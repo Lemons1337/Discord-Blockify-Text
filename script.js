d38000aabc1353da75b80fa89461a52b2d7fedd8
Lemons <24715897+Lemons1337@users.noreply.github.com>
2020-01-27 21:07:00 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function blockify(str) {
+    var prefix = ':regional_indicator_';
+    var suffix = ': ';
+
+    str = str.toLowerCase();
+
+    return str.replace(/[a-z]/gi, char => {
+        return prefix.concat(char, suffix);
+    });
+}
+
+var send = XMLHttpRequest.prototype.send;
+XMLHttpRequest.prototype.send = function(data) {
+    var obj;
+
+    try {
+        obj = JSON.parse(data);
+    } catch (err) {}
+
+    if (obj && obj.content) {
+        obj.content = blockify(obj.content);
+        data = JSON.stringify(obj);
+    }
+
+    return send.apply(this, [data]);
+}
